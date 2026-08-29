dd0c1ca6261675993c9907ab4957355571b63931
stefi <stefania.enescu.17@gmail.com>
2026-08-29 20:33:49 +0300

    Am adaugat HelloWorld

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Stefi");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    //single line comment
+    /*
+
+     */
+}
+
+
